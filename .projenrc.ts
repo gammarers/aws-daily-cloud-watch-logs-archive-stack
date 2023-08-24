bc49fb472a6f0077cb22529a1e886deda9ad9189
yicr <yicr@users.noreply.github.com>
2023-08-24 16:28:36 +0900

    fix: jsii version to ~5.0.0

--- .projenrc.ts
+++ .projenrc.ts
@@ -4,7 +4,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   authorAddress: 'yicr@users.noreply.github.com',
   cdkVersion: '2.80.0',
   typescriptVersion: '4.9.x',
-  jsiiVersion: '5.1.x',
+  jsiiVersion: '~5.0.0',
   defaultReleaseBranch: 'main',
   name: '@gammarer/aws-daily-cloud-watch-logs-archiver',
   projenrcTs: true,
