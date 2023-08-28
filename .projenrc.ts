0dced7fc7460aa45fed084a1637d616a65876195
yicr <yicr@users.noreply.github.com>
2023-08-28 15:38:30 +0900

    chore: deps upgrade schedule set to every wednesday 19:00

--- .projenrc.ts
+++ .projenrc.ts
@@ -53,7 +53,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   depsUpgradeOptions: {
     workflowOptions: {
       labels: ['auto-approve', 'auto-merge'],
-      schedule: javascript.UpgradeDependenciesSchedule.expressions(['0 19 * * *']),
+      schedule: javascript.UpgradeDependenciesSchedule.expressions(['0 19 * * 3']), // every wednesday 19:00 (JST/THU:0400)
     },
   },
   autoApproveOptions: {
