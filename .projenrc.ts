84fee39257f597a1f38218c0443caad8f8b365a9
yicr <yicr@users.noreply.github.com>
2024-01-19 12:00:07 +0900

    chore(deps): constructs version set 10.0.5

--- .projenrc.ts
+++ .projenrc.ts
@@ -3,6 +3,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   author: 'yicr',
   authorAddress: 'yicr@users.noreply.github.com',
   cdkVersion: '2.80.0',
+  constructsVersion: '10.0.5',
   typescriptVersion: '5.1.x',
   jsiiVersion: '5.1.x',
   defaultReleaseBranch: 'main',
