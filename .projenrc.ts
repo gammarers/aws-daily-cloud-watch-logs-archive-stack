a4181f61396995e9e858a85167dbd27acfa06945
yicr <yicr@users.noreply.github.com>
2024-08-15 10:55:52 +0900

    feat: workflow node version 22.4.x

--- .projenrc.ts
+++ .projenrc.ts
@@ -52,7 +52,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   releaseToNpm: true,
   npmAccess: javascript.NpmAccess.PUBLIC,
   minNodeVersion: '18.0.0',
-  workflowNodeVersion: '20.11.0',
+  workflowNodeVersion: '22.4.x',
   depsUpgradeOptions: {
     workflowOptions: {
       labels: ['auto-approve', 'auto-merge'],
