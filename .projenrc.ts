64aef141c742b8b1c98b091cca5427c1bcb74e74
yicr <yicr@users.noreply.github.com>
2025-04-22 20:12:11 +0900

    chore(deps): update mypackages & lambda node version to 22.x

--- .projenrc.ts
+++ .projenrc.ts
@@ -13,8 +13,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   keywords: ['aws', 'cdk', 'aws-cdk', 'scheduler', 's3', 'bucket', 'archive', 'lambda'],
   majorVersion: 2,
   deps: [
-    '@gammarers/aws-secure-log-bucket@~2.0.7',
-    '@gammarers/aws-secure-bucket@~2.0.8',
+    '@gammarers/aws-secure-log-bucket@^2.1.19',
   ],
   devDeps: [
     'aws-sdk-client-mock@^3',
@@ -24,8 +23,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
     '@gammarers/jest-aws-cdk-asset-filename-renamer@~0.5.8',
   ],
   peerDeps: [
-    '@gammarers/aws-secure-log-bucket@~2.0.7',
-    '@gammarers/aws-secure-bucket@~2.0.8',
+    '@gammarers/aws-secure-bucket@^2.1.0',
   ],
   compat: true,
   jestOptions: {
@@ -41,7 +39,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   },
   lambdaOptions: {
     // target node.js runtime
-    runtime: awscdk.LambdaRuntime.NODEJS_20_X,
+    runtime: awscdk.LambdaRuntime.NODEJS_22_X,
     bundlingOptions: {
       // list of node modules to exclude from the bundle
       externals: ['@aws-sdk/client-cloudwatch-logs'],
@@ -55,7 +53,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   depsUpgradeOptions: {
     workflowOptions: {
       labels: ['auto-approve', 'auto-merge'],
-      schedule: javascript.UpgradeDependenciesSchedule.expressions(['0 19 * * 3']), // every wednesday 19:00 (JST/THU:0400)
+      schedule: javascript.UpgradeDependenciesSchedule.expressions(['0 19 * * 3']),
     },
   },
   autoApproveOptions: {
