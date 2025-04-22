b5672be0966f8f5d6fa0873f4d0584ce064d793f
yicr <yicr@users.noreply.github.com>
2025-04-22 19:12:31 +0900

    feat(deps): cdk version set to 2.189.1 security update

--- .projenrc.ts
+++ .projenrc.ts
@@ -2,7 +2,7 @@ import { awscdk, javascript } from 'projen';
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'yicr',
   authorAddress: 'yicr@users.noreply.github.com',
-  cdkVersion: '2.80.0',
+  cdkVersion: '2.189.1',
   constructsVersion: '10.0.5',
   typescriptVersion: '5.4.x',
   jsiiVersion: '5.4.x',
