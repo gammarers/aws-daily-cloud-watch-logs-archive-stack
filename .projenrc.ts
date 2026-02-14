77d4ece3c6d317fdf5463cc50e336bdc07dc19ae
yicr <yicr@users.noreply.github.com>
2026-02-14 03:32:06 +0000

    chore: never upgarde

--- .projenrc.ts
+++ .projenrc.ts
@@ -52,7 +52,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   depsUpgradeOptions: {
     workflowOptions: {
       labels: ['auto-approve', 'auto-merge'],
-      schedule: javascript.UpgradeDependenciesSchedule.expressions(['0 19 * * 3']),
+      schedule: javascript.UpgradeDependenciesSchedule.NEVER,
     },
   },
   autoApproveOptions: {
